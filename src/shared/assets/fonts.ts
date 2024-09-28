b10a7ec683157de418ee6558d41cd7f05a276bc5
litheee <alexxx547@gmail.com>
2024-09-28 13:03:14 +0300

    commit

--- src/shared/assets/fonts.ts
+++ src/shared/assets/fonts.ts
@@ -3,17 +3,20 @@ import { Inter, Noto_Sans, Roboto } from 'next/font/google'
 export const inter = Inter({
   weight: ['400', '500', '700'],
   subsets: ['latin'],
-  variable: '--font-inter'
+  variable: '--font-inter',
+  display: 'swap'
 })
 
 export const notoSans = Noto_Sans({
   weight: ['400'],
   subsets: ['latin'],
-  variable: '--font-noto-sans'
+  variable: '--font-noto-sans',
+  display: 'swap'
 })
 
 export const roboto = Roboto({
   weight: ['400', '500'],
   subsets: ['latin'],
-  variable: '--font-roboto'
+  variable: '--font-roboto',
+  display: 'swap'
 })
